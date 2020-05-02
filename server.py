a7f70fb3ff06a398ff37fdc9350cd73c480fa31c
norantharowat <norantharowat@gmail.com>
2020-05-02 21:11:21 +0200

    update

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import mysql.connector
-from flask import Flask, redirect, url_for, request,render_template,flash
-
+from flask import Flask, redirect, url_for, request,render_template,flash, send_file
+from io import BytesIO
+import os
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -9,6 +10,7 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER']  = "C:/Users/Lenovo/CMMS/static/uploads"
 @app.route('/')
 def index():
 	return render_template("index.html")
@@ -30,6 +32,7 @@ def Equipments():
       'header':row_headers
       }
       return render_template("Equipments.html",data=data)
+      # return render_template("Equipments.php",data=data)
 
     else :
       mycursor.execute("SELECT * FROM Equipment WHERE Name= '" +equipment+ "' AND Installation_date = '" +installation+ "' AND Warranty_expires = '" +warranty+ "' ")
@@ -41,6 +44,7 @@ def Equipments():
       'header':row_headers
       }
       return render_template("Equipments.html",data=data)
+      # return render_template("Equipments.php",data=data)
 
   else:
     mycursor.execute("SELECT * FROM Equipment")
@@ -52,6 +56,7 @@ def Equipments():
     'header':row_headers
     }
     return render_template("Equipments.html",data=data)
+    # return render_template("Equipments.php",data=data)
 
 @app.route('/addEquipment', methods=['POST','GET'])
 def addEquipment():
@@ -69,14 +74,80 @@ def addEquipment():
     Floor = request.form["Floor"]
     Department = request.form["Department"]
     Scrapping_date = request.form["Scrapping_date"]
-    sql = "INSERT INTO Equipment(Asset_ID, Name ,Serial_number ,Model_number,Model_name,Manufacturer,Installation_date,Warranty_expires,Facility,Building,Floor,Department,Scrapping_date) VALUES (%s,%s,%s, %s ,%s ,%s,%s, %s ,%s,%s,%s,%s,%s )"
-    val = (Asset_ID, Name ,Serial_number ,Model_number,Model_name,Manufacturer,Installation_date,Warranty_expires,Facility,Building,Floor,Department,Scrapping_date)
+    filedata = request.files['Data_sheet'] 
+    # sheet = filedata.filename
+    sheet = Name+'_'+Asset_ID+'.pdf'
+    uploads_dir = "C:/Users/Lenovo/CMMS/static/uploads"
+    filedata.save(os.path.join(uploads_dir, sheet))  
+    sql = "INSERT INTO Equipment(Asset_ID, Name ,Serial_number ,Model_number,Model_name,Manufacturer,Installation_date,Warranty_expires,Facility,Building,Floor,Department,Scrapping_date,Data_sheet) VALUES (%s,%s,%s, %s ,%s ,%s,%s, %s ,%s,%s,%s,%s,%s,%s )"
+    val = (Asset_ID, Name ,Serial_number ,Model_number,Model_name,Manufacturer,Installation_date,Warranty_expires,Facility,Building,Floor,Department,Scrapping_date, sheet)
     mycursor.execute(sql, val)
     mydb.commit() 
     return render_template("/addEquipment.html")
 	
   else:
     return render_template("/addEquipment.html")
+ 
+@app.route('/parts', methods=['POST','GET'])
+def parts():
+
+  if request.method == 'POST':
+    Name = request.form["Name"]
+    print(Name)
+    if Name != '' :
+      mycursor.execute("SELECT Inventory.Part_Number,Inventory.Name,Inventory.Asset_ID,Inventory.Vendor,Inventory.Cost,Inventory.Quantity,Equipment.Name as Asset_Name FROM `Inventory` JOIN `Equipment` WHERE Equipment.Asset_ID=Inventory.Asset_ID AND Inventory.Name = '" +Name+ "'") 
+      row_headers=[x[0] for x in mycursor.description] 
+      myresult = mycursor.fetchall()
+      data={
+      'message':"data retrieved",
+      'rec':myresult,
+      'header':row_headers
+      }
+    else:
+      mycursor.execute("SELECT Inventory.Part_Number,Inventory.Name,Inventory.Asset_ID,Inventory.Vendor,Inventory.Cost,Inventory.Quantity,Equipment.Name as Asset_Name FROM `Inventory` JOIN `Equipment` WHERE Equipment.Asset_ID=Inventory.Asset_ID")
+      row_headers=[x[0] for x in mycursor.description] 
+      myresult = mycursor.fetchall()
+      data={
+      'message':"data retrieved",
+      'rec':myresult,
+      'header':row_headers
+      }
+
+    return render_template("parts.html",data=data)
+
+  else:
+    mycursor.execute("SELECT Inventory.Part_Number,Inventory.Name,Inventory.Asset_ID,Inventory.Vendor,Inventory.Cost,Inventory.Quantity,Equipment.Name as Asset_Name FROM `Inventory` JOIN `Equipment` WHERE Equipment.Asset_ID=Inventory.Asset_ID")
+    row_headers=[x[0] for x in mycursor.description] 
+    myresult = mycursor.fetchall()
+    data={
+    'message':"data retrieved",
+    'rec':myresult,
+    'header':row_headers
+    }
+    return render_template("parts.html",data=data)
+
+@app.route('/addParts' , methods=['POST','GET'] )
+def addParts():
+ if request.method == 'POST':
+   Part_Number = request.form["Part_Number"]
+   Name = request.form["Name"]
+   Asset_ID = request.form["Asset_ID"]
+   Vendor = request.form["Vendor"]
+   Cost = request.form["Cost"]
+   Quantity = request.form["Quantity"]
+   sql = "INSERT INTO Inventory(Part_Number, Name ,Asset_ID ,Vendor,Cost,Quantity) VALUES (%s,%s,%s, %s ,%s ,%s)"
+   val = (Part_Number, Name ,Asset_ID ,Vendor,Cost,Quantity)
+   mycursor.execute(sql, val)
+   mydb.commit() 
+    
+   return render_template("addParts.html")
+
+ else:
+
+   return render_template("addParts.html")
+	
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
